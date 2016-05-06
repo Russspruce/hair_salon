44efaea2642e721fe9ab76ec389b6f86dc31ef2c
Andrew Russo <russspruce@gmail.com>
2016-05-06 15:59:39 -0700

    Completed first part of front end testing and creating forms to allow one to login to the program and see thier schedule.  Next part will be working of the form for the clients.

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,58 @@
+import org.sql2o.*;
+import org.junit.*;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Bonjour!");
+  }
+
+  @Test
+  public void stylistIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Login"));
+    fill("#styler").with("Johnny Bravo");
+    submit(".btn");
+    assertThat(pageSource()).contains("You have been logged in!");
+  }
+
+  @Test
+  public void stylerIsDisplayedTest() {
+    Stylist myStylist = new Stylist("Johnny Bravo");
+    myStylist.save();
+    String stylistPath = String.format("http://localhost:4567/stylists/%d", myStylist.getId());
+    goTo(stylistPath);
+    assertThat(pageSource()).contains("Johnny Bravo");
+  }
+
+  @Test
+  public void confirmToddOnMainPage() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Login"));
+    fill("#styler").with("Sweeny Todd");
+    submit(".btn");
+    goTo("http://localhost:4567/stylists");
+    assertThat(pageSource()).contains("Sweeny Todd");
+  }
+}
